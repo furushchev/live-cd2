a62f5e104541a54ef1149c3b5c91f221f9017b49
Kei Okada <kei.okada@gmail.com>
2016-04-08 21:34:45 +0900

    use echo color, not tput

--- color.mk
+++ color.mk
@@ -1,7 +1,5 @@
 #
 # http://eli.thegreenplace.net/2013/12/18/makefile-functions-and-color-output
 define colorecho
-      @tput setaf 6
-      @echo [$@] $1
-      @tput sgr0
+      @/bin/echo -ne "\e[1;32m[$@]\e[1;34m $1\e[m\n"
 endef
